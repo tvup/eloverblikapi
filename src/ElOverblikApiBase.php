40d2e28d0fd7d076b03775306d40f02947415a28
Torben Evald Hansen <tvupper@gmail.com>
2022-09-16 05:53:12 +0200

    accessing debug doesn't work for now

--- src/ElOverblikApiBase.php
+++ src/ElOverblikApiBase.php
@@ -157,9 +157,9 @@ class ElOverblikApiBase
             'headers' => ['Authorization' => 'Bearer ' . $this->accessToken],
         ];
 
-        if ($this->debug) {
-            $options = array_merge($options, ['debug' => true,]);
-        }
+//        if ($this->debug) {
+//            $options = array_merge($options, ['debug' => true,]);
+//        }
         if (null !== $payload) {
             $options = array_merge($options, ['json' => $payload]);
         }
